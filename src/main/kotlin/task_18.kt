d90232203ba64d952255435e84307fb1e822ae70
Tihomir Chobanov <tihomir.chobanov@gmail.com>
2023-01-04 18:39:56 +0200

    Commit #18

---
+++ src/main/kotlin/task_18.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    val a = intArrayOf(2, 1, 1, 2)
+    val b = intArrayOf(3, 4, 4, 2, 5)
+    val c = a + b
+    for (i in c) print("$i ")
+
+
+}
+
+/*
+18. Join the arrays A(N) and B(M) and form the array C(N+M) by complementing A with B.
+ */
